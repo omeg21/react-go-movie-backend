6f73838f1a6fc7b8ab93b28fc056878f0ac7b5d8
omega <Muhammadomega02@gmail.com>
2023-10-12 11:14:43 +0700

    done with JWT moving on to CRUD

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -109,4 +111,57 @@ func (j *Auth) GetExpiredRefreshCookie() *http.Cookie {
 		SameSite: http.SameSiteStrictMode,
 		Secure:   true,
 	}
+}
+
+func (j *Auth) GetTokenFromHeaderAndVerify(w http.ResponseWriter, r *http.Request) (string, *Claims, error) {
+
+	w.Header().Add("Vary", "Authorization")
+
+	//get auth header
+	authHeader := r.Header.Get("Authorization")
+
+	//sanity check
+	if authHeader == "" {
+		return "", nil, errors.New("no auth header")
+	}
+
+	//Split header on spaces
+	headerParts := strings.Split(authHeader, " ")
+	if len(headerParts) != 2 {
+		return "", nil, errors.New("invalid auth header")
+	}
+
+	// chech to see if we have the word Bearer
+	if headerParts[0] != "Bearer" {
+		return "", nil, errors.New("invalid auth header no bearer")
+	}
+
+	token := headerParts[1]
+
+	//declare an empty claims
+	claims := &Claims{}
+
+	//parse the token
+
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(j.Secret), nil
+	})
+
+	if err != nil {
+		if strings.HasPrefix(err.Error(), "token is expired by") {
+			return "", nil, errors.New("expired token")
+		}
+		return "", nil, err
+	}
+
+	if claims.Issuer != j.Issuer {
+		return "", nil, errors.New("invalid issuer")
+	}
+
+	return token, claims, nil
+
 }
